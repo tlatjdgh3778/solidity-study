9c914991092e68725208a58b2716e923538e6530
tlatjdgh3778 <tjdgh3778@gmail.com>
2025-05-28 21:55:57 +0900

    Interacting with contracts ABI

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -6,9 +6,22 @@ import { SimpleStorage } from "./SimpleStorage.sol";
 contract StorageFactory {
     // SimpleStorage SimpleStorage 라는 타입
     // simpleStorage 변수
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Address
+        // ABI(Application Binary Interface)
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    } 
 }
