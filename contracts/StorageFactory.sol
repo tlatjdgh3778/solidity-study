f9d6bf73096cc4bd16cb66be0003586c80ad57fa
tlatjdgh3778 <tjdgh3778@gmail.com>
2025-05-28 21:34:07 +0900

    update

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.24;
 import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    // SimpleStorage 변수
-    // simpleStorage 계약
+    // SimpleStorage SimpleStorage 라는 타입
+    // simpleStorage 변수
     SimpleStorage public simpleStorage;
 
     function createSimpleStorageContract() public {
