c50e6f92efb9edd0784acc84ec5cbbec45fd1e36
tlatjdgh3778 <tjdgh3778@gmail.com>
2025-05-30 21:51:19 +0900

    solidity revert

--- contracts/remix-fundme/FundMe.sol
+++ contracts/remix-fundme/FundMe.sol
@@ -9,12 +9,21 @@
 pragma solidity ^0.8.24;
 
 contract FundMe {
+    // payable 붙은 함수나 주소만 이더를 전송 받을 수 있
     function fund() public payable  {
         // Allow users to send money
         // Have a minimum money sent
         // 1. How do we send ETH to this contract ?
-        // 전역 변수 msg
+        // 전역 변수 msg, msg.value 로 전송된 이더 금액을 확인할 수 있
         require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1,000,000,000,000,000,000 = 1 * 10 ** 18
+        // 조건을 만족하지 않으면, 트랜잭션은 revert (되돌리기)됨
+        // 거래가 revert 되면, 이 트랜잭션의 모든 상태 변경이 취소된다.
+        // 단, 사용자는 사용한 가스는 환불받지 못한다.
+
+        // What is revert ?
+        // Undo any actions that have been done, and send the remaining gas back
+        // 지금까지 실행된 작업들을 되돌리고, 남은 가스를 사용자에게 반환함
+
     }
 
     function widthraw() public {}
